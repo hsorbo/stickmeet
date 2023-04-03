80360b90de4140fbaf4b6396513fae6d37465396
Håvard Sørbø <havard@hsorbo.no>
2023-04-02 22:09:25 -0500

    Mnemo V2

--- contrib/mnemo_import.py
+++ contrib/mnemo_import.py
@@ -9,6 +9,11 @@ from serial.tools import list_ports
 import time
 from datetime import datetime
 import sys
+from enum import Enum
+
+class Version(Enum):
+    V1 = 1
+    V2 = 2
 
 def to_signed(byte) -> int:
     integer = int(byte.hex(), 16)
@@ -17,18 +22,22 @@ def to_signed(byte) -> int:
         return integer
     return integer - 256
 
-def import_data(device, verbose=False):
+def import_data(device, version : Version, verbose : bool = False):
     ser = serial.Serial(device, baudrate=9600, timeout=1, bytesize=8, stopbits=1, parity='N')
     ser.flushInput()
     ser.flushOutput()
-    ser.write(b'C')
-    time.sleep(.1)
-    now = datetime.now()
-
-    for d in [ "%y","%m", "%d","%H", "%M" ]:
-        date_component = now.strftime(d)
-        date_as_byte = chr(int(date_component)).encode()
-        ser.write(date_as_byte)
+    
+    if version == Version.V1:
+        ser.write(b'C')
+        time.sleep(.1)
+        now = datetime.now()
+        for d in [ "%y","%m", "%d","%H", "%M" ]:
+            date_component = now.strftime(d)
+            date_as_byte = chr(int(date_component)).encode()
+            ser.write(date_as_byte)
+    elif version == Version.V2:
+        ser.write(b'getdata\n')
+        time.sleep(.1)
 
     dump_file = []
     c = 0
@@ -63,14 +72,16 @@ def format_dmp(raw_data): return ';'.join(str(e) for e in raw_data) + ";"
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--tty", help="tty of the device to read from (e.g. /dev/ttyUSB0)")
+    parser.add_argument("--mnemo-version", help="Mnemo version (1 or 2), defaults to 1", type=int, default=1)
     parser.add_argument("--output", help="output file to write to")
     args = parser.parse_args()
     tty = args.tty if args.tty else detect()
+    version = Version(args.mnemo_version)
     verbose = True if args.output else False
     if not tty:
         sys.stderr.write("No tty specified/detected\n")
         exit(1)
-    data = import_data(tty, verbose)
+    data = import_data(tty, version, verbose)
     if len(data) == 0:
         sys.stderr.write("No data found\n")
         exit(1)
